f6d2446e39f54849190c0a7e944b2c53d2898923
asharMehmood2211004KHICND <ashar.mehmood@xloopdigital.com>
2022-12-19 15:23:31 +0500

    updated styles

--- src/components/DisplayPokemon.js
+++ src/components/DisplayPokemon.js
@@ -1,29 +1,20 @@
 import React from 'react'
-
+import pokemonlogo from '../logo.png'
 export const DisplayPokemon = ({poke}) => {
   return (
-    
     <>
-    { poke != '' ? 
- 
+       <img className='pokelogo' src={pokemonlogo} />
+    { poke != '' ?
     <div className='detail '>
-        
-       
         <img src={poke.sprites.other.home.front_default} alt="pokemon image"  />
-        <h1>{poke.name}</h1> 
+        <h1>{poke.name}</h1>
         {/* <p> {poke.stats.stat.name}  </p> */}
         {/* <h3> {poke.moves.move.name} </h3> */}
         <h3> weight : {poke.weight}</h3>
         <h3> height : {poke.height}</h3>
         <h3> abilities: {poke.abilities[0].ability.name} </h3>
-    </div> 
-    
-    : '' } 
-    
+    </div>
+    : '' }
     </>
-
-    
- 
-
   )
 }
